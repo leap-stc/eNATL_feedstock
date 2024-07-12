9e27af0435f5a426dda78c697ba0aca821748d3c
Julius Busecke <julius@ldeo.columbia.edu>
2024-07-12 16:07:16 +0100

    Update eNATL60.py

--- feedstock/eNATL60.py
+++ feedstock/eNATL60.py
@@ -60,7 +60,7 @@ class Preprocess(beam.PTransform):
 
 eNATL60_BLBT02 = (
     beam.Create(pattern.items())
-    | OpenURLWithFSSpec(max_concurrency=2)
+    | OpenURLWithFSSpec(max_concurrency=1)
     | OpenWithXarray(
         xarray_open_kwargs={"use_cftime": True, "engine": "netcdf4"},
         load=True,
