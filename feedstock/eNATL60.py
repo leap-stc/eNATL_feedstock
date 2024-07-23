42587662efcb4259610f91bf342325c0c0360599
Julius Busecke <julius@ldeo.columbia.edu>
2024-07-23 12:10:17 +0200

    Update eNATL60.py

--- feedstock/eNATL60.py
+++ feedstock/eNATL60.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import xarray as xr
 import apache_beam as beam
 from leap_data_management_utils.data_management_transforms import (
@@ -14,6 +15,8 @@ from pangeo_forge_recipes.transforms import (
     OpenWithXarray,
     StoreToZarr,
 )
+
+logger = logging.getLogger(__name__)
 
 # parse the catalog store locations (this is where the data is copied to after successful write (and maybe testing)
 catalog_store_urls = get_catalog_store_urls("feedstock/catalog.yaml")
@@ -45,11 +48,12 @@ class Preprocess(beam.PTransform):
 
     @staticmethod
     def _set_coords(ds: xr.Dataset) -> xr.Dataset:
+        logger.info(f"Before preprocessing {ds=}")
         t_new = xr.DataArray(ds.time_counter.data, dims=["time"])
         ds = ds.assign_coords(time=t_new)
         ds = ds.drop(["time_counter"])
         ds = ds.set_coords(["deptht", "depthw", "nav_lon", "nav_lat", "tmask"])
-
+        logger.info(f"After preprocessing {ds=}")
         return ds
 
     def expand(self, pcoll: beam.PCollection) -> beam.PCollection:
