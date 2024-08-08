f60775c9d4d497f0546037a132de9fddf277aaa5
Raphael Hagen <norlandrhagen@gmail.com>
2024-08-08 17:05:00 -0600

    noprune

--- configs/config_dataflow.py
+++ configs/config_dataflow.py
@@ -19,3 +19,5 @@ c.TargetStorage.fsspec_class = "gcsfs.GCSFileSystem"
 c.InputCacheStorage.fsspec_class = "gcsfs.GCSFileSystem"
 c.TargetStorage.root_path = f"gs://leap-scratch/data-library/feedstocks/output/{FEEDSTOCK_NAME}/{{job_name}}"
 c.InputCacheStorage.root_path = f"gs://leap-scratch/data-library/feedstocks/cache"
+
+
