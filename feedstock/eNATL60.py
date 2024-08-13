39bfabec64334c2e71f6124d4d0050ba30e73eb9
Raphael Hagen <norlandrhagen@gmail.com>
2024-08-13 17:49:04 -0600

    pooch

--- feedstock/eNATL60.py
+++ feedstock/eNATL60.py
@@ -1,5 +1,6 @@
 import xarray as xr
 import apache_beam as beam
+import pooch
 from pangeo_forge_recipes.patterns import pattern_from_file_sequence
 from pangeo_forge_recipes.transforms import (
     ConsolidateMetadata,
@@ -21,10 +22,19 @@ dataset_url = "https://zenodo.org/records/10513552/files"
 
 ## Monthly version
 input_urls = [
-    f"{dataset_url}/eNATL60-BLBT02_y2009m07d{d:02d}.1d_TSWm_60m.nc#mode=bytes"
-    for d in days
+    f"{dataset_url}/eNATL60-BLBT02_y2009m07d{d:02d}.1d_TSWm_60m.nc" for d in days
 ]
 pattern = pattern_from_file_sequence(input_urls, concat_dim="time")
+
+
+class OpenWithPooch(beam.PTransform):
+    @staticmethod
+    def _open_pooch(url: str) -> str:
+        # import pdb; pdb.set_trace()
+        return pooch.retrieve(url=url, known_hash=None)
+
+    def expand(self, pcoll: beam.PCollection) -> beam.PCollection:
+        return pcoll | "open" >> beam.MapTuple(lambda k, v: (k, self._open_pooch(v)))
 
 
 class Preprocess(beam.PTransform):
@@ -50,12 +60,11 @@ class Preprocess(beam.PTransform):
 eNATL60BLBT02 = (
     beam.Create(pattern.items())
     # | OpenURLWithFSSpec(max_concurrency=1)
-    | OpenWithXarray(
-        xarray_open_kwargs={"engine": "netcdf4"}
-        # xarray_open_kwargs={"use_cftime": True, "engine": "netcdf4"},
-        # load=True,
-        # copy_to_local=True,
-    )
+    | OpenWithPooch()
+    | OpenWithXarray()
+    # xarray_open_kwargs={"use_cftime": True, "engine": "netcdf4"},
+    # load=True,
+    # copy_to_local=True,)
     | Preprocess()
     | StoreToZarr(
         store_name="eNATL60-BLBT02.zarr",
