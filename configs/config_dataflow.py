accbf4bcc71fa1f07eec62ae0ef33f40165b8d84
Raphael Hagen <norlandrhagen@gmail.com>
2024-10-21 17:59:53 -0600

    disable autoscale, num worker =5 + bump mem

--- configs/config_dataflow.py
+++ configs/config_dataflow.py
@@ -7,10 +7,10 @@ FEEDSTOCK_NAME = repo_path.split('/')[-1]
 c.Bake.prune = False
 c.Bake.bakery_class = "pangeo_forge_runner.bakery.dataflow.DataflowBakery"
 c.DataflowBakery.use_dataflow_prime = False
-c.DataflowBakery.machine_type = "t2d-standard-32"
+c.DataflowBakery.machine_type = "t2d-standard-48"
 c.DataflowBakery.disk_size_gb = 400
-c.DataflowBakery.num_workers = 10
-c.DataflowBakery.max_num_workers = 50
+c.DataflowBakery.autoscaling_algorithm = "NONE"
+c.DataflowBakery.num_workers = 5
 c.DataflowBakery.use_public_ips = True
 c.DataflowBakery.service_account_email = (
     "leap-community-bakery@leap-pangeo.iam.gserviceaccount.com"
