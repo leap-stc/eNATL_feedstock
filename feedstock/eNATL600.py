ebeb6883aa428f1dccab8ec43f20df7f070e3d7a
Raphael Hagen <norlandrhagen@gmail.com>
2024-10-21 17:34:03 -0600

    updates standard recipe to read bytes

--- feedstock/eNATL600.py
+++ feedstock/eNATL600.py
@@ -1,13 +1,11 @@
 import xarray as xr
 import pandas as pd
 import apache_beam as beam
-import pooch
 from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
 from pangeo_forge_recipes.transforms import (
     ConsolidateMetadata,
     ConsolidateDimensionCoordinates,
     OpenWithXarray,
-    OpenURLWithFSSpec,
     StoreToZarr,
 )
 
@@ -24,21 +22,11 @@ dates = pd.date_range("2009-07-01", "2010-06-30", freq="D")
 
 def make_full_path(time):
     date_fmt = time.strftime("y%Ym%md%d")
-    return f"https://ige-meom-opendap.univ-grenoble-alpes.fr/thredds/fileServer/meomopendap/extract/MEOM/eNATL60/eNATL60-BLBT02/1d/eNATL60/eNATL60-BLBT02_{date_fmt}.1d_TSWm_600m.nc"
+    return f"https://nyu1.osn.mghpcc.org/leap-pangeo-manual/eNATL_600m/eNATL60-BLBT02_{date_fmt}.1d_TSWm_600m.nc#mode=bytes"
 
 
 time_concat_dim = ConcatDim("time", dates)
 pattern = FilePattern(make_full_path, time_concat_dim)
-# pattern = pattern.prune()
-
-
-class OpenWithPooch(beam.PTransform):
-    @staticmethod
-    def _open_pooch(url: str) -> str:
-        return pooch.retrieve(url=url, known_hash=None)
-
-    def expand(self, pcoll: beam.PCollection) -> beam.PCollection:
-        return pcoll | "open" >> beam.MapTuple(lambda k, v: (k, self._open_pooch(v)))
 
 
 class Preprocess(beam.PTransform):
@@ -61,8 +49,9 @@ class Preprocess(beam.PTransform):
 
 eNATL600BLBT02 = (
     beam.Create(pattern.items())
-    | OpenURLWithFSSpec()
-    | OpenWithXarray(load=True, copy_to_local=True)
+    | OpenWithXarray(
+        xarray_open_kwargs={"engine": "netcdf4"}
+    )  # h5netcdf fails to load time
     | Preprocess()
     | StoreToZarr(
         store_name="eNATL600m-BLBT02.zarr",
